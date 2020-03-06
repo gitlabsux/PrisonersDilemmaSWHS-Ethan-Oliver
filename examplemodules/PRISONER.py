58a7749e7ae069f7d75b04e4e9cf8c7888837f7c
LEE OLIVER L <olee2201@mpsedu.org>
2020-03-06 10:14:40 -0600

    Add stuff

--- examplemodules/PRISONER.py
+++ examplemodules/PRISONER.py
@@ -6,9 +6,15 @@ strategy_description = 'A secret strargegy'
 
 
 def move(my_history, their_history, my_score, their_score, opponent_name):
+    tooManyBetrays = 0
     if their_history[-1:] == 'b':
+        if their_history[-3:] == 'b':       # If opponent betrays 3 times in a row, start to only betray (see line 17)
+            tooManyBetrays = 1
+        else:                               # If opponent betrays once, betray next turn
+            return 'b'
+    elif their_history == 6:                # ALWAYS BETRAY ON ROUND SEVEMN!!!!!!!!!!!
         return 'b'
-    elif their_history == 6:
+    elif tooManyBetrays == 1:
         return 'b'
-    else:
+    else:                                   # Otherwise, collude
         return 'c'
